e8a67e6bd175334f8c0368b2e4b240642fe30300
Ghazi Khan <mailme.gkhan@gmail.com>
2022-03-20 15:35:11 +0530

    Implemented get method

--- First-Project/server.js
+++ First-Project/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    const response = `<h1>Hey Ghazi, how u doing!</h1>`
+    res.send(response);
+})
+
 app.listen(3000, () => {
     console.log('Server started..');
 });
+
+
