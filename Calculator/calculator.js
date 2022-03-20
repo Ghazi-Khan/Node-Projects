474e0b199d8a088ac6f0edfc04fbadeb05b01c5a
Ghazi Khan <mailme.gkhan@gmail.com>
2022-03-20 16:35:53 +0530

    Sending html form as response from server

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -3,9 +3,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const responseFileName = __dirname + '/index.html';
+    // res.send('Hello World!');
+    res.sendFile(responseFileName);
+
 });
 
 app.listen(3000, () => {
     console.log('Server started...');
 });
+
+
+//  What we  have learnt in this project :
+// Summary =>
+
+// 1. To send a file as a response we have to use sendFile()
+// 2. We shlould use not use relative path
+// 3. __direName, will give the directory path
