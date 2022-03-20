cba11aa9490821ee418519c3450d3082856f2b83
Ghazi Khan <mailme.gkhan@gmail.com>
2022-03-20 15:47:35 +0530

    Understanding and Working with Routes

--- First-Project/server.js
+++ First-Project/server.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const app = express();
 
+// Understanding and Working with Routes
+
+// home page
 app.get('/', (req, res) => {
-    const response = `<h1>Hey Ghazi, how u doing!</h1>`
+    const response = `<h1>Hello World!</h1>`
     res.send(response);
+});
+
+//  contact
+app.get('/contact', (req, res) => {
+    res.send('contact me at mailme.gkhan@gmail.com');
 })
+
+// about
+app.get('/about', (req, res) => {
+    res.send('Hey, Ghazi khan is here, I am a developer!, who loves to write code a lot');
+});
 
 app.listen(3000, () => {
     console.log('Server started..');
 });
-
-
